a45798413d2388512b0001efb3bb8f46f0a41b5c
Costas Tyfoxylos <costas.tyf@gmail.com>
2019-03-19 14:03:18 +0100

    Slight cleanup and refactoring to extend browser compatibility. Added firefox, not working yet.

--- mapscookiegettercli/library/cookiegetter.py
+++ mapscookiegettercli/library/cookiegetter.py
@@ -35,10 +35,12 @@ import logging
 import sys
 import pickle
 from pathlib import Path
+from time import sleep
 
-import webdriver_manager
+from requests import Session
 
 from mapscookiegettercli.mapscookiegettercliexceptions import UnsupportedOS, UnsupportedDefaultBrowser
+from mapscookiegettercli.browsers import Chrome, Firefox
 
 __author__ = '''Costas Tyfoxylos <costas.tyf@gmail.com>'''
 __docformat__ = '''google'''
@@ -63,6 +65,8 @@ MAPS_LOGIN = ('https://accounts.google.com/signin/v2/identifier?'
               'flowName=GlifWebSignIn&'
               'flowEntry=ServiceLogin')
 
+LOGGED_IN_HEURISTIC = 'See travel times, traffic and nearby places'
+
 
 class CookieGetter:  # pylint: disable=too-few-public-methods
     """Object able to retrieve the cookies from an interactive login session to a google maps service"""
@@ -75,12 +79,11 @@ class CookieGetter:  # pylint: disable=too-few-public-methods
         self._logger.info('Identified OS as %s', self.os)
         self.default_browser = self._identify_default_browser(self.os)
         self._logger.info('Identified default browser as %s', self.default_browser)
-        self.drivers_path = Path(__file__).parent.joinpath('..' '/drivers')
-        self._logger.info('Set selenium drivers path as "%s"', self.drivers_path)
 
     @staticmethod
     def _identify_os():
         platforms = {
+            'linux': 'linux',
             'linux1': 'linux',
             'linux2': 'linux',
             'darwin': 'mac',
@@ -100,7 +103,7 @@ class CookieGetter:  # pylint: disable=too-few-public-methods
     @staticmethod
     def _identify_browser_mac():
         from plistlib import load, FMT_BINARY  # pylint: disable=no-name-in-module
-        supported_browsers = ('firefox', 'safari', 'chrome', 'opera')
+        supported_browsers = ('firefox', 'chrome')
         default_browser_plist_path = ('~/Library/Preferences/com.apple.LaunchServices/'
                                       'com.apple.launchservices.secure.plist')
         path = Path(default_browser_plist_path).expanduser()
@@ -117,7 +120,7 @@ class CookieGetter:  # pylint: disable=too-few-public-methods
     @staticmethod
     def _identify_browser_windows():
         from winreg import HKEY_CURRENT_USER, HKEY_CLASSES_ROOT, OpenKey, QueryValueEx  # pylint: disable=import-error
-        supported_browsers = ('firefox', 'safari', 'chrome', 'opera', 'edge', 'ie')
+        supported_browsers = ('firefox', 'chrome', 'edge', 'ie')
         default_browser_registry_path = (r'Software\Microsoft\Windows\Shell\Associations'
                                          r'\UrlAssociations\https\UserChoice')
         with OpenKey(HKEY_CURRENT_USER, default_browser_registry_path) as key:
@@ -137,18 +140,23 @@ class CookieGetter:  # pylint: disable=too-few-public-methods
     @staticmethod
     def _identify_browser_linux():
         from subprocess import Popen, PIPE
-        supported_browsers = ('firefox', 'chrome', 'chromium', 'opera', 'firefox')
-        command = ['xdg-settingsa', 'get', 'default-web-browser']
+        supported_browsers = ('firefox', 'chrome')
+        command = ['xdg-settings', 'get', 'default-web-browser']
         try:
             get_browser_command = Popen(command, stdout=PIPE, stderr=PIPE)
         except FileNotFoundError:
-            print('Could not execute xdg-settings, propably unsupported version of linux.')
+            print('Could not execute xdg-settings, probably unsupported version of linux.')
             return 'unknown'
         output, _ = get_browser_command.communicate()
         browser = next((browser for browser in supported_browsers
                         if browser in output.decode('utf-8')),
                        'unknown')
         return browser
+
+    def _get_driver(self):
+        browsers = {'chrome': Chrome,
+                    'firefox': Firefox}
+        return browsers.get(self.default_browser)()
 
     def run(self, cookie_file_name='location_sharing.cookies'):
         """Executes the process and saves the cookies
@@ -159,32 +167,14 @@ class CookieGetter:  # pylint: disable=too-few-public-methods
         Returns:
 
         """
-        self._bootstrap_browser()
-        from selenium import webdriver
-        from selenium.webdriver.chrome.options import Options
-        versions = webdriver_manager.versions(self.drivers_path, [self.default_browser])
-        driver_name = versions['chromedriver'][0][1]
-        path_to_chromedriver = Path(self.drivers_path) / driver_name
-        chrome_options = Options()
-        chrome_options.add_argument('--disable-extensions')
-        chrome_options.add_argument('--profile-directory=Default')
-        chrome_options.add_argument('--incognito')
-        chrome_options.add_argument('--disable-plugins-discovery')
-        chrome_options.add_argument('--start-maximized')
-        chrome_options.add_argument('--disable-infobars')
-        self._logger.info('Starting up chrome driven by selenium')
-        driver = webdriver.Chrome(executable_path=path_to_chromedriver, chrome_options=chrome_options)
-        self._logger.info('Deleting all cookies')
-        driver.delete_all_cookies()
+        driver = self._get_driver()
         self._logger.info('Starting interactive login process.')
         driver.get(MAPS_LOGIN)
-        from time import sleep
-        while 'See travel times, traffic and nearby places' not in driver.page_source:
+        while LOGGED_IN_HEURISTIC not in driver.page_source:
             sleep(0.5)
         self._logger.info('Log in successful, getting session cookies.')
-        from requests import Session
         session = Session()
-        self._logger.info('Transfering cookies to a requests session.')
+        self._logger.info('Transferring cookies to a requests session.')
         for cookie in driver.get_cookies():
             for invalid in ['httpOnly', 'expiry']:
                 try:
@@ -197,9 +187,5 @@ class CookieGetter:  # pylint: disable=too-few-public-methods
             pickle.dump(session.cookies, ofile)
         self._logger.info('Terminating browser session.')
         driver.close()
-
+        # TODO properly handly exception raised if user closes browser before finishing process
         # selenium.common.exceptions.NoSuchWindowException: Message: no such window: window was already closed
-
-    def _bootstrap_browser(self):
-        self._logger.info('Bootstrapping selenium by downloading appropriate driver')
-        webdriver_manager.update(self.default_browser, self.drivers_path)
